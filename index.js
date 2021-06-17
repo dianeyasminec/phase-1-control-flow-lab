b014c5427fd04404bb8dc1a4dec97e6e556f4374
dianeyasminec <dianeyasminec60@gmail.com>
2021-06-17 17:51:39 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
+  if (feet <= 400 ){
+   return "This one is on me!" 
+  }
+  else if  (feet>2000 && feet<2500){ 
+    return "I will gladly take your thirty bucks."
+  }
+  else if (feet>2500){
+    return "No can do."
+  }
+}
+  
+  // Write your code here!
+scuberGreetingForFeet(199) 
+
+function ternaryCheckCity(city){
+  return (city=="NYC")? "Ok, sounds good." : "No go." 
   // Write your code here!
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+function switchOnCharmFromTip(tipamount){
+var tip;
+  switch (tipamount) {
+  case 'generous':
+    tip="Thank you so much.";
+    break;
+  case'not as generous' :
+    tip= "Thank you.";
+    break;
 
-function switchOnCharmFromTip(){
+  case 'thanks for everything':
+    tip="Bye."
+    break;
+  }
+   return tip;
+} 
+
   // Write your code here!
-}
